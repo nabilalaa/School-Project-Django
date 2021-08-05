684a08c5b5e0c64e800c2bf33f041ed60bff4e1a
nabilalaa <nabilalaa06@gmail.com>
2021-08-05 16:30:30 +0200

    school

--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import *
 from .form import ResultForm
+from django.contrib import messages
 
 
 def test(request):
-
     num = request.POST.get("number")
+    if request.POST:
+        if not Result.objects.filter(number=num):
+            print("d")
+            messages.error(request, "not found")
 
     context = {
         "results": Result.objects.filter(number=num),
